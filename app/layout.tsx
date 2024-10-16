5da32fd9cf452e046e84e57fa843255157f0d965
Kazuma Endo <endo.kazuma1027@gmail.com>
2024-10-16 13:35:02 +0900

    フォントを変更

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,22 @@
-import { GeistSans } from "geist/font/sans";
 import { ThemeProvider } from "next-themes";
+import { Inter } from "next/font/google";
+import { Noto_Sans_JP } from "next/font/google";
 import "./globals.css";
 import { Footer } from "@/components/footer";
 import { Header } from "@/components/header";
+
+const inter = Inter({
+	subsets: ["latin"],
+	display: "swap",
+	variable: "--font-inter",
+});
+
+const notoSansJP = Noto_Sans_JP({
+	weight: ["400", "700"],
+	subsets: ["latin"],
+	display: "swap",
+	variable: "--font-noto-sans-jp",
+});
 
 const defaultUrl = process.env.VERCEL_URL
 	? `https://${process.env.VERCEL_URL}`
@@ -20,8 +34,12 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<html lang="ja" className={GeistSans.className} suppressHydrationWarning>
-			<body className="bg-background text-foreground">
+		<html
+			lang="ja"
+			suppressHydrationWarning
+			className={`${inter.variable} ${notoSansJP.variable}`}
+		>
+			<body className="bg-background text-foreground font-sans">
 				<ThemeProvider
 					attribute="class"
 					defaultTheme="system"
@@ -31,7 +49,7 @@ export default function RootLayout({
 					<main className="min-h-screen flex flex-col items-center w-full">
 						<div className="flex-1 w-full flex flex-col items-center">
 							<Header />
-							<div className="flex flex-col gap-20 max-w-5xl p-5 w-full">
+							<div className="flex flex-col gap-20 max-w-5xl p-5 w-full items-center">
 								{children}
 							</div>
 							<Footer />
